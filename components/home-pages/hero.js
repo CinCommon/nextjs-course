1ff57543d78efef7cec30fbc4cfa8621c682a6f5
尹子凡 <CinCommon@users.noreply.github.com>
2023-07-02 23:42:54 +0800

    feat. add Blog components

---
+++ components/home-pages/hero.js
@@ -0,0 +1,15 @@
+import classes from './hero.modules.css'
+
+const Hero = () => {
+    return (
+        <section>
+            <div>Image</div>
+            <h1>Hi, I'm Steven</h1>
+            <p>
+                I Blog about DevOps
+            </p>
+        </section>
+    )
+}
+
+export default Hero;
