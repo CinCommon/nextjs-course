7ebc7a926a91bc03d5feb24c2ba7236a3b1778b4
尹子凡 <CinCommon@users.noreply.github.com>
2023-07-08 23:13:41 +0800

    feat. add some css

--- components/home-pages/hero.js
+++ components/home-pages/hero.js
@@ -2,11 +2,11 @@ import classes from './hero.module.css'
 
 const Hero = () => {
     return (
-        <section>
-            <div>Image</div>
+        <section className={classes.hero}>
+            <div className={classes.image}>Image</div>
             <h1>Hi, I'm Steven</h1>
             <p>
-                I Blog about DevOps
+                I Blog about DevOps and Languages Knowledge.
             </p>
         </section>
     )
